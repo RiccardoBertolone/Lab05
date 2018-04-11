21d6c20a686fecd21c4e0ceb792086a7386ba639
rbert <rbert@172.22.15.131>
2018-04-11 09:11:46 +0200

    Versione iniziale: completata parte grafica

---
+++ Lab5_Anagrammi/src/it/polito/tdp/anagrammi/model/Model.java
@@ -0,0 +1,10 @@
+package it.polito.tdp.anagrammi.model;
+
+import java.util.*;
+
+public class Model {
+	
+	List<String> soluzioni = new ArrayList<String>() ; // elenco delle soluzioni
+	List<Character> lettere = new ArrayList<Character>() ; // elenco delle lettere utilizzabili
+
+}
