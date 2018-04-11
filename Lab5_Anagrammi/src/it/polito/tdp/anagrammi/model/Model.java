184e3ba2a41d81abdedcef29002a99110f8a9d7e
rbert <rbert@172.22.15.131>
2018-04-11 11:03:56 +0200

    versione algoritmo di ricorsione implementato

--- Lab5_Anagrammi/src/it/polito/tdp/anagrammi/model/Model.java
+++ Lab5_Anagrammi/src/it/polito/tdp/anagrammi/model/Model.java
@@ -4,7 +4,49 @@ import java.util.*;
 
 public class Model {
 	
-	List<String> soluzioni = new ArrayList<String>() ; // elenco delle soluzioni
-	List<Character> lettere = new ArrayList<Character>() ; // elenco delle lettere utilizzabili
+	List<String> soluzioni ; // elenco delle soluzioni
+	Map<Character, Integer> lettere ; // elenco delle lettere utilizzabili
+	
+	public List<String> calcolaPermutazioni(String s) {
+		soluzioni = new ArrayList<String>() ;
+		lettere = new TreeMap<Character, Integer>() ;
+		for (int i = 0; i<s.length(); i++) {
+			lettere.put(s.charAt(i), punteggio(s.charAt(i), s)) ;
+		}
+		recursive("", s.length()) ;
+		return soluzioni ;
+	}
+
+	private Integer punteggio(char charAt, String s) {
+		int count = 0 ;
+		for(int i=0; i<s.length(); i++) {
+			if (s.charAt(i)==charAt)
+				count++ ;
+		}
+		return count ;
+	}
+
+	private void recursive(String parziale, int lunghezza) {
+		if (parziale.length()==lunghezza) {
+			soluzioni.add(parziale) ;
+			System.out.println(parziale) ;
+			return ;
+		}
+		for (Character c : lettere.keySet()) {
+			parziale+=c.toString();
+			if (isCorretta(parziale))
+				recursive(parziale, lunghezza) ;
+			StringBuilder sb = new StringBuilder(parziale) ;
+			parziale=sb.deleteCharAt(parziale.length()-1).toString() ;
+		}
+	}
+
+	public boolean isCorretta(String parziale) {
+		boolean flag = true ;
+		for (Character c : lettere.keySet())
+			if (punteggio(c, parziale) > lettere.get(c))
+				flag = false ;
+		return flag;
+	}
 
 }
